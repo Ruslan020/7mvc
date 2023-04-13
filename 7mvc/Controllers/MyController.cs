c7b2f079428c8f2ebbc14b7673f87180c37ccab3
РУСЛАН <@LAPTOP-A6O46K3I>
2023-04-13 12:51:18 +0300

    Добавьте файлы проекта.

---
+++ 7mvc/Controllers/MyController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace _7mvc.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/ 
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        
+        public IActionResult TestApi(string number)
+        {
+            if(number == null)
+            {
+                Random rnd = new Random();
+                number = rnd.Next(0, 100).ToString();
+            }
+            string interestingFact = getData(number);
+
+            ViewData["Number"] = number;
+            ViewData["Message"] = interestingFact;
+            return View();
+        }
+
+        private string getData(string number)
+        {
+            string res = "";
+            string sURL;
+            sURL = "http://numbersapi.com/" + number +"/trivia";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    res += sLine;
+            }
+
+            return res;
+        }
+    }
+}
